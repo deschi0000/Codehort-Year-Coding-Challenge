de1041e759bf5acd2beb55f770e7826925fdbc1f
deschi0000 <deschi0000@outlook.com>
2024-09-19 22:19:39 +0800

    rework and clean-up

--- Week 1 - Challenge Build Your Own wc Tool!/new-wc.py
+++ Week 1 - Challenge Build Your Own wc Tool!/new-wc.py
@@ -2,92 +2,76 @@ import os
 import sys 
 import regex as re
 
-file = ""
-command = None
 
-if len(sys.argv) > 2:
-    file = sys.argv[2]
-    command = sys.argv[1].strip("-")
-else:
-    file = sys.argv[1]
+def count_bytes(file_path):         # -c (count bytes)
+    return str(os.path.getsize(file_path))
 
-full_path = os.path.join(os.getcwd(), file)
+def count_lines(file):              # -l (count lines)
+    return len(file.splitlines())
 
-
-def count_bytes():
-    return str(os.path.getsize(full_path))
-
-def count_words():
-    words = 0
-    with open(full_path, 'r', encoding='utf-8') as my_file:
-        # words = re.findall(r'\b\w+\b', my_file.read())
-        # TODO: Regex still not getting all the correct cases
-        # pattern = re.compile(r'\b_[a-zA-Z]+_\b|\b\d+[a-z]+\b|\b(?:https?://)?(?:www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(?:/[a-zA-Z0-9\-._~:/?#\[\]@!$&\'()*+,;=]*)?\b|\b[a-zA-Z]+’[a-z]\b|[a-zA-Z]\.[a-zA-Z]\.|\d\.[A-Z]\.\d\b|\$\d+\,\d+|\b\d+\b|\b[a-zA-Z\'\-]+\b|\b\d+[-\d]*\b|\b\w\b', re.UNICODE)
-        pattern = r'\S+'
-        words = re.findall(pattern, my_file.read())
-        
-        # print(words)
-
-        # # or
-        # with open(full_path, 'r', encoding='utf-8') as my_file:
-        #     text = my_file.read()
-        #     pattern = re.compile(r'\p{L}+', re.UNICODE) 
-        #     matches = pattern.findall(text)
-
-        # print(f'{len(matches)} {file}')
+def count_words(file):              # -w (count words)
+    pattern = re.compile(r'\S+', re.UNICODE)
+    words = re.findall(pattern, file)
     return len(words)
 
-def count_lines():
-    lines = 0
-    with open(full_path, 'r', encoding='utf-8') as my_file:
-        for line in my_file:
-            lines += 1  
-    return lines
-
-def count_chars():
-    with open(full_path, 'r', encoding='utf-8') as my_file:
-        chars = 0
-        for line in my_file:
-            print(len(line))
-            print(line)
-            chars += len(line)
-        return chars
+def count_chars(file):              # -m (count characters)
+    return len(file)
 
 
 def main():
+
+    file = ""
+    command = None
+
+    # If there is no argument, instruct the user
+    if len(sys.argv) < 2:
+        print("Usage: new-wc [-c | -l | -w | -m] [filename]")
+        return
+
+    if len(sys.argv) > 2:
+        # If there is a command
+        file = sys.argv[2]
+        command = sys.argv[1].strip("-")
+    else:
+        # If there is no command
+        file = sys.argv[1]
+
+        # Check if a file is provided and exists
+    if file and os.path.isfile(file):
+        full_path = os.path.join(os.getcwd(), file)
+        with open(full_path, 'r', encoding='utf-8') as my_file:
+            file_content = my_file.read()
+
     if not command:
     # TODO default (no arg) combine -c,-l,-w
     # > new-wc test.txt
     #    7145   58164  342190 test.txt
-        print(f'{count_bytes()} {count_lines()} {count_words()} {file}')
+        print(f'{count_bytes(full_path)} {count_lines(file_content)} {count_words(file_content)} {file}')
 
     elif command == "c":
-        # TODO -c (count bytes)
+        # -c (count bytes)
         # > new-wc -c test.txt
         #    342190 test.txt
-        print(f'{count_bytes()} {file}')
+        print(f'{count_bytes(full_path)} {file}')
 
     elif command == "l":
-        # TODO -l (count lines)
+        # -l (count lines)
         # > new-wc -l test.txt
         #    7145 test.txt
-        print(f'{count_lines()} {file}')
+        print(f'{count_lines(file_content)} {file}')
 
     elif command == "w":
-        # TODO -w (count words)
+        # -w (count words)
         # > new-wc -w test.txt
         #    58164 test.txt
-        print(f'{count_words()} {file}')
+        print(f'{count_words(file_content)} {file}')
 
     elif command == "m":
-        # TODO -m (count characters)
+        # -m (count characters)
         # > new-wc -m test.txt
         #    339292 test.txt
-        print(f'{count_chars()} {file}')
-
-
-# if no file:
-
+        # print(f'{count_chars(full_path)} {file}')
+        print(f'{count_chars(file_content)} {file}')
 
 
 if __name__ == '__main__':
