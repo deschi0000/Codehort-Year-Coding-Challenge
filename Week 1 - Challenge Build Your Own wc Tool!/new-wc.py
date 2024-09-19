900fad4c8174fbdbe06b97da02f6b71fd7e8ba42
deschi0000 <deschi0000@outlook.com>
2024-09-19 17:37:11 +0800

    clean up, change regex. add main()

--- Week 1 - Challenge Build Your Own wc Tool!/new-wc.py
+++ Week 1 - Challenge Build Your Own wc Tool!/new-wc.py
@@ -2,14 +2,6 @@ import os
 import sys 
 import regex as re
 
-# file = sys.argv[1]
-# full_path = os.getcwd() + '\\' + sys.argv[1]
-
-# print(full_path)
-
-# with open(full_path, 'r', encoding='utf-8') as my_file:
-#     print(my_file.read())
-
 file = ""
 command = None
 
@@ -19,7 +11,7 @@ if len(sys.argv) > 2:
 else:
     file = sys.argv[1]
 
-full_path = os.getcwd() + '\\' + file
+full_path = os.path.join(os.getcwd(), file)
 
 
 def count_bytes():
@@ -30,10 +22,10 @@ def count_words():
     with open(full_path, 'r', encoding='utf-8') as my_file:
         # words = re.findall(r'\b\w+\b', my_file.read())
         # TODO: Regex still not getting all the correct cases
-        # pattern = r'\b[a-zA-Z\'\-]+\b|[A-Z](?:\.[A-Z])|\b\d+[-\d]*\b'
-        pattern = re.compile(r'\b_[a-zA-Z]+_\b|\b\d+[a-z]+\b|\b(?:https?://)?(?:www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(?:/[a-zA-Z0-9\-._~:/?#\[\]@!$&\'()*+,;=]*)?\b|\b[a-zA-Z]+’[a-z]\b|[a-zA-Z]\.[a-zA-Z]\.|\d\.[A-Z]\.\d\b|\$\d+\,\d+|\b\d+\b|\b[a-zA-Z\'\-]+\b|\b\d+[-\d]*\b', re.UNICODE)
         # pattern = re.compile(r'\b_[a-zA-Z]+_\b|\b\d+[a-z]+\b|\b(?:https?://)?(?:www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(?:/[a-zA-Z0-9\-._~:/?#\[\]@!$&\'()*+,;=]*)?\b|\b[a-zA-Z]+’[a-z]\b|[a-zA-Z]\.[a-zA-Z]\.|\d\.[A-Z]\.\d\b|\$\d+\,\d+|\b\d+\b|\b[a-zA-Z\'\-]+\b|\b\d+[-\d]*\b|\b\w\b', re.UNICODE)
+        pattern = r'\S+'
         words = re.findall(pattern, my_file.read())
+        
         # print(words)
 
         # # or
@@ -45,14 +37,12 @@ def count_words():
         # print(f'{len(matches)} {file}')
     return len(words)
 
-
 def count_lines():
     lines = 0
     with open(full_path, 'r', encoding='utf-8') as my_file:
         for line in my_file:
             lines += 1  
     return lines
-
 
 def count_chars():
     with open(full_path, 'r', encoding='utf-8') as my_file:
@@ -64,44 +54,44 @@ def count_chars():
         return chars
 
 
-# TODO -c (count bytes)
-# > new-wc -c test.txt
-#    342190 test.txt
-if not command:
-   print(f'{count_bytes()} {count_lines()} {count_words()} {file}')
+def main():
+    if not command:
+    # TODO default (no arg) combine -c,-l,-w
+    # > new-wc test.txt
+    #    7145   58164  342190 test.txt
+        print(f'{count_bytes()} {count_lines()} {count_words()} {file}')
 
-elif command == "c":
-    print(f'{count_bytes()} {file}')
+    elif command == "c":
+        # TODO -c (count bytes)
+        # > new-wc -c test.txt
+        #    342190 test.txt
+        print(f'{count_bytes()} {file}')
 
-# TODO -l (count lines)
-# > new-wc -l test.txt
-#    7145 test.txt
-elif command == "l":
-    print(f'{count_lines()} {file}')
+    elif command == "l":
+        # TODO -l (count lines)
+        # > new-wc -l test.txt
+        #    7145 test.txt
+        print(f'{count_lines()} {file}')
 
-# TODO -w (count words)
-# > new-wc -w test.txt
-#    58164 test.txt
-elif command == "w":
-    print(f'{count_words()} {file}')
+    elif command == "w":
+        # TODO -w (count words)
+        # > new-wc -w test.txt
+        #    58164 test.txt
+        print(f'{count_words()} {file}')
 
-# TODO -m (count characters)
-# > new-wc -m test.txt
-#    339292 test.txt
-elif command == "m":
-    print(f'{count_chars()} {file}')
+    elif command == "m":
+        # TODO -m (count characters)
+        # > new-wc -m test.txt
+        #    339292 test.txt
+        print(f'{count_chars()} {file}')
 
-
-
-
-
-# TODO default (no arg) combine -c,-l,-w
-# > new-wc test.txt
-#    7145   58164  342190 test.txt
 
 # if no file:
 
 
+
+if __name__ == '__main__':
+    main()
 
 
 
