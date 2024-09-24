3cef1def5f6693a36c4afcbbe293e7d8cfc5e3a0
deschi0000 <deschi0000@outlook.com>
2024-09-24 13:15:24 +0800

    update

--- Week 2 - Challenge Build Your Own JSON Parser!/json_parser.py
+++ Week 2 - Challenge Build Your Own JSON Parser!/json_parser.py
@@ -1,6 +1,21 @@
 import os
 
 def main():
+    # TODO:
+    # fail 3 - keys must be quoted
+    # fail 12 - illegal invocation
+    # fail 18 - too deep
+    # fail 20 - double colon
+    # fail 22 - colon instead of comma
+    # fail 23 - bad value
+    # fail 24 - single quote
+    # fail 25 - tab characters
+    # fail 26 - tab/ chars
+    # fail 27 - Line/ break
+    # fail 28 - Line break
+    # fail 29 - [0e]
+    # fail 30 - [0e+]
+    # fail 31 - [0e+-1]
     
     folder_names = ['unit-test', 'test-files']
 
@@ -10,7 +25,7 @@ def main():
     print("PATH:    " + file_path)
 
     pass_file = os.path.join(file_path, 'pass2.json')
-    fail_file = os.path.join(file_path, 'fail14.json')   
+    fail_file = os.path.join(file_path, 'fail33.json')   
     
     with open(fail_file, 'r') as file:
     # with open(pass_file, 'r') as file:
@@ -18,7 +33,10 @@ def main():
         #     print(f)
         # print(file.read())
         file = file.read().strip()
+        print("==============================================")
         print(file)
+        print("==============================================")
+
         # print(file[0])
         # if file[0] == "{" or file[0] == "[":
         #     print("match")
@@ -64,18 +82,45 @@ def main():
 
             # TODO: Nums cannot be Hex
             # From reading the file, every number is a string!
-            if type(file[i]) == int:
-                print("Its an int")
+            # if type(file[i]) == int:
+            #     print("Its an int")
 
+            # TODO: Extra comma
+            if file[i] == ",":
+                # if file[i+1] or file[i-1] not in ("\"", " "):
+                #     return "Invalid Json: Extra Comma"
+                arr = file.split(",")
+                check_extra_comma_arr = [i.strip() for i in arr]
+                print(check_extra_comma_arr)
+                for i in check_extra_comma_arr:
+                    if i in ("[","]","{","}"):
+                        return "Invalid Json: Extra Comma"
+            
+            # TODO: Check for leading zeros 
+            if file[i] == "0":
+                if file[i+1] in ("1","2","3","4","5","6","7","8","9"):
+                    return "Invalid Json: Leading Zeros"
+
+                
         # print("Array Counter: " + str(array_counter))
         # print("Bracket Counter: " + str(bracket_counter))
 
         if array_counter != 0:
-            return "Invalid Json: Not enough closing arrays"
+            return "Invalid Json: Not enough closing arrays / Mismatch"
         elif bracket_counter != 0:
-            return "Invalid Json: Not enough closing brackets"
+            return "Invalid Json: Not enough closing brackets / Mismatch"
         
-        
+        # TODO: Check for Illegal expressions
+        check_illegal_expression_arr = [i.strip() for i in file.split(":")]
+        for i in check_illegal_expression_arr:
+            if "\"" not in i:
+                if "+" in i or "-" in i or "*" in i or "/" in i:
+                    return "Invalid Json: Illegal Expression"
+                
+
+
+
+
     # RULES
     # Json must start be enclosed with [], or {}
     # 
